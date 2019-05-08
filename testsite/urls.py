79256260386b18e599891a8b095f9fd6892879e0
Marc Kirkwood <human@unboundlocal.com>
2019-05-08 16:16:07 +0100

    Added Django-Rest-Swagger for API docs (comes with DRF).

--- testsite/urls.py
+++ testsite/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from rest_framework_swagger.views import get_swagger_view
+
+
+schema_view = get_swagger_view(title='Starship API')
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'swagger-docs/', schema_view),   
 ]
